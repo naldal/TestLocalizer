d2f397c80fb81efc876629c92ef9654e4be9730b
naldal <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>
2023-06-08 21:29:55 +0900

    Initial Commit

---
+++ TestLocalizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLocalizer
+//
+//  Created by 송하민 on 2023/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
